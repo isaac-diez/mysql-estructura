49578760c09fbe4853d1157ab161f2d2e046d7cf
Isaac <isaac.diez@proton.me>
2025-02-13 19:06:09 +0100

    Delete pizzeria_populateDB.sql

--- pizzeria_populateDB.sql
+++
@@ -1,6 +0,0 @@
-SELECT SUM(Pizzeria.order_items.order_quantity) AS total_orders_sold
-FROM Pizzeria.order_items
-JOIN Pizzeria.orders ON Pizzeria.order_items.orders_orders_id = Pizzeria.orders.orders_id
-JOIN Pizzeria.employees ON Pizzeria.orders.orders_employees_id = Pizzeria.employees.employees_id
-WHERE Pizzeria.employees.employees_name = 'Maria';
-
